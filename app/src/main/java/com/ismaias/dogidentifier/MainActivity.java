1ef412a04af0da14ae561eaa5e8066d4ba06e982
Isaias Tello <ismael.tello@masclicks.com.mx>
2021-07-31 15:03:42 -0500

    Creación de pantalla de inicio

--- app/src/main/java/com/ismaias/dogidentifier/MainActivity.java
+++ app/src/main/java/com/ismaias/dogidentifier/MainActivity.java
@@ -2,7 +2,10 @@ package com.ismaias.dogidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+    public void registroUsuario(View v) {
+        Toast.makeText(this, "Esta función aún no está disponible en este momento", Toast.LENGTH_LONG).show();
+    }
+
+
+    public void sendToLogin(View view) {
+        Intent intent = new Intent(this, UserLogin.class);
+        startActivity(intent);
+    }
+
 }
